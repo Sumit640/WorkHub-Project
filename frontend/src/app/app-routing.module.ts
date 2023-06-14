e630d078c2e3f92fa94e69ae1656ae082f06e3e0
Sumit640 <Sumit03012000@gmail.com>
2023-06-14 17:11:04 +0530

    Revert "Added routes to starting pages"
    This reverts commit ac3e9568864364ab55c962ea9dbbe9f273304e7e.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,15 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FacilityComponent } from './facility/facility.component';
-import { AboutComponent } from './about/about.component';
-import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full'},
-  { path: 'home', component: FacilityComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'login', component: LoginComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
